c89473dd48b2f1003ac0534e23dbcc3e857c1f5c
SanketTops <sanket.tops@gmail.com>
2023-12-27 17:40:37 +0530

    First Program added

---
+++ Module-1/first.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter value of A:");
+	scanf("%d",&a);
+	printf("Enter value of B:");
+	scanf("%d",&b);
+	
+	if(a<b) //true
+	{
+		printf("A is min");
+	}
+	else
+	{
+		printf("A is max");
+	}
+}
